97bcafa7862d0e2ccec1583c627d66306b1b0bbf
mari-potter <136430267+mari-potter@users.noreply.github.com>
2023-07-03 22:32:54 +0900

    Create score.php

---
+++ kadai_005/score.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題_005</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 合計点をだす
+    echo '合計点';
+    echo'<br>';
+    echo 80+60+55+40+100+25+80+95+30+60;
+    // 平均点をだす
+    echo '<br>';
+    echo'平均点';
+    echo'<br>';
+    echo 625/10;
+    ?>
+</p>
+</body>
+</html>
