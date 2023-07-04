f3e92c4f508aca20c80f6c5a1ba3bcdc054999db
mari-potter <136430267+mari-potter@users.noreply.github.com>
2023-07-04 16:10:22 +0900

    Update score.php

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,16 +9,27 @@
 <body>
   <p>
     <?php
-    // 合計点をだす
-    echo '合計点';
-    echo'<br>';
-    echo 80+60+55+40+100+25+80+95+30+60;
-    // 平均点をだす
-    echo '<br>';
-    echo'平均点';
-    echo'<br>';
-    echo 625/10;
+    $score1='80';
+    $score2='60';
+    $score3='55';
+    $score4='40';
+    $score5='100';
+    $score6='25';
+    $score7='80';
+    $score8='95';
+    $score9='30';
+    $score10='60';
+
+    // 合計点をだす　合計用の変数$sum
+    $sum=$score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10;
+    
+    // 平均点をだす 平均用の変数$ave
+
+    $ave=$sum/10;
+    echo $ave;
+
     ?>
 </p>
 </body>
+
 </html>
